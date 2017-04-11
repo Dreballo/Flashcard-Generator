5f714038fb590f8dbf321af185d529d9350f2ede
Andre Seballlo <andreseballlo@Stacys-MacBook-Air.local>
2017-04-11 13:42:25 -0400

    complete game all modes work

--- app.js
+++ app.js
@@ -3,9 +3,6 @@
 	2. use inquirer-js to determine whether we make new basic cards, new cloze cards or play game.
 	3. use conditionals to build objects from constructors based on users choice
 	4. functions to call back questions and validate
-	//add a method validate the cloze entry (throw the error)
-	//lookup JSON Validator
-	//Add NPM package for Request
 */
 
 //Variables for requirements at launch of server application
@@ -22,14 +19,12 @@ var incorrect = 0;
 
 //Function to Start application prompts
 var pickMode = function() {
-    inquire.prompt([
-        {
-            name: "start",
-            message: "How would you like to proceed?",
-            type: "list",
-            choices: ["Create Basic Flashcards", "Create Cloze Flashcards", "Take A Basic Quiz", "Take A Cloze Quiz"]
-        }
-    ]).then(function(answer) {
+    inquire.prompt([{
+        name: "start",
+        message: "How would you like to proceed?",
+        type: "list",
+        choices: ["Create Basic Flashcards", "Create Cloze Flashcards", "Take A Basic Quiz", "Take A Cloze Quiz"]
+    }]).then(function(answer) {
         var command = answer.start;
         switch (command) {
             case "Create Basic Flashcards":
@@ -37,14 +32,16 @@ var pickMode = function() {
                 createBasicCard();
                 break;
             case "Create Cloze Flashcards":
-            	// call a function that sets up inquire prompts for creating cloze card
+                // call a function that sets up inquire prompts for creating cloze card
                 createClozeCard();
                 break;
             case "Take A Basic Quiz":
                 //Reads the BasicLog.txt and returns Quiz Questions
+                basicQuiz(0);
                 break;
             case "Take A Cloze Quiz":
                 //Reads the Clozelog.txt and returns Quiz Questions
+                clozeQuiz(0);
                 break;
             default:
                 console.log('YOU BROKE MY APP');
@@ -79,12 +76,11 @@ var createBasicCard = function() {
 //Launches Inquirer for next card
 var nextBasicCard = function() {
     inquire.prompt([{
-            name: "next",
-            message: "Do you want to create another card?",
-            type: "confirm",
-            default: true
-        }
-    ]).then(function(answer) {
+        name: "next",
+        message: "Do you want to create another card?",
+        type: "confirm",
+        default: true
+    }]).then(function(answer) {
         if (answer.next != true) {
             pickMode();
         } else {
@@ -109,22 +105,103 @@ var createClozeCard = function() {
         //sets up next question
         return nextClozeCard();
     })
- 
+
 }
 
 //function to create another cloze card
-var nextClozeCard = function(){
-	inquire.prompt([{
-            name: "next",
-            message: "Do you want to create another card?",
-            type: "confirm",
-            default: true
-        }
-    ]).then(function(answer) {
+var nextClozeCard = function() {
+    inquire.prompt([{
+        name: "next",
+        message: "Do you want to create another card?",
+        type: "confirm",
+        default: true
+    }]).then(function(answer) {
         if (answer.next != true) {
             pickMode();
         } else {
             createClozeCard();
         }
     })
+}
+
+//function to launch basic quiz
+var basicQuiz = function(count) {
+    fs.readFile('BasicLog.json', 'utf8', function(err, data) {
+
+        var questionData = JSON.parse(data);
+
+      
+        if (count < questionData.length) {
+            inquire.prompt([{
+                name: "question",
+                message: questionData[count].front,
+                type: "input"
+            }]).then(function(answer) {
+                if (answer.question.toLowerCase() === questionData[count].back.toLowerCase()) {
+                    console.log('Correct');
+                    correct++;
+                    count++;
+                    basicQuiz(count);
+                } else {
+                    console.log('Incorrect');
+                    incorrect++;
+                    console.log("The correct answer is: " + questionData[count].back);
+                    count++;
+                    basicQuiz(count);
+                }
+            })
+        } else {
+            showScore();
+            pickMode();
+        }
+
+    })
+}
+
+
+//function to launch cloze quiz
+var clozeQuiz = function(count) {
+    fs.readFile('ClozeLog.json', 'utf8', function(err, data) {
+
+        var questionData = JSON.parse(data);
+
+
+
+      
+        if (count < questionData.length) {
+
+        	 var newClozeCard = new ClozeCard(questionData[count].text, questionData[count].cloze);
+
+            inquire.prompt([{
+                name: "question",
+                message: newClozeCard.partialText(),
+                type: "input"
+            }]).then(function(answer) {
+                if (answer.question.toLowerCase() === newClozeCard.cloze.toLowerCase()) {
+                    console.log('Correct');
+                    correct++;
+                    count++;
+                    clozeQuiz(count);
+                } else {
+                    console.log('Incorrect');
+                    incorrect++;
+                    console.log("The correct answer is: " + newClozeCard.text);
+                    count++;
+                    clozeQuiz(count);
+                }
+            })
+        } else {
+            showScore();
+            pickMode();
+        }
+
+    })
+}
+
+//displays the score
+var showScore = function() {
+    console.log("--------------------");
+    console.log('correct: ' + correct);
+    console.log('incorrect: ' + incorrect);
+    console.log("--------------------");
 }
